0cf8b521aa82b337267f389fa1049c3690935a29
Caio Victor <caiopvasconcelos@gmail.com>
2025-10-21 18:33:13 -0300

    fix: ajustando mock e layout

---
+++ capcred-ui/src/app/core/mocks/reports.mock.ts
@@ -0,0 +1,26 @@
+export const REPORTS_MOCK = [
+  {
+    id: 1,
+    name: 'Relatório Financeiro Mensal',
+    type: 'Financeiro',
+    date: '01/10/2025',
+  },
+  {
+    id: 2,
+    name: 'Relatório de Pagamentos',
+    type: 'Pagamentos',
+    date: '05/10/2025',
+  },
+  {
+    id: 3,
+    name: 'Relatório de Empréstimos',
+    type: 'Empréstimos',
+    date: '15/10/2025',
+  },
+  {
+    id: 4,
+    name: 'Relatório de Clientes Ativos',
+    type: 'Clientes',
+    date: '20/10/2025',
+  },
+];

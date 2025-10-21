11d24cc0ff6e8fec1da3bd399ab9a01aff8a491f
Caio Victor <caiopvasconcelos@gmail.com>
2025-10-21 17:19:50 -0300

    feat: implementando responsividade e ajustes no header

--- capcred-ui/src/app/ui/components/header/header.ts
+++ capcred-ui/src/app/ui/components/header/header.ts
@@ -1,20 +1,21 @@
-import { Component, HostListener, signal } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output, signal } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
-  scrolled = signal(false);
+  @Input() isMobile = false;
+  @Output() menuClicked = new EventEmitter<void>();
 
-  @HostListener('window:scroll', [])
-  onScroll() {
-    this.scrolled.set(window.scrollY > 10);
+  onMenuClick() {
+    this.menuClicked.emit();
   }
 }

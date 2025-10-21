c5e86a5e3f62fb78476a7bd50b536d3eeff03595
Caio Victor <caiopvasconcelos@gmail.com>
2025-10-21 15:52:47 -0300

    feat: iniciando layout

--- capcred-ui/src/app/ui/layouts/shell-layout/shell-layout.ts
+++ capcred-ui/src/app/ui/layouts/shell-layout/shell-layout.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
+import { Sidebar } from '../../components/sidebar/sidebar';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-shell-layout',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterOutlet,
+    Sidebar,
+    Header,
+  ],
   templateUrl: './shell-layout.html',
-  styleUrl: './shell-layout.scss'
+  styleUrls: ['./shell-layout.scss'],
 })
 export class ShellLayout {
 

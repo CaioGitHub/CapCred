fd198b80ec5af0fb381bcef227a01ec3c08ff6e3
Caio Victor <caiopvasconcelos@gmail.com>
2025-10-21 15:52:47 -0300

    feat: iniciando layout

--- capcred-ui/src/app/app.routes.ts
+++ capcred-ui/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ShellLayout } from './ui/layouts/shell-layout/shell-layout';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellLayout,
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
+  },
+];
--- capcred-ui/src/app/ui/components/sidebar/sidebar.ts
+++ capcred-ui/src/app/ui/components/sidebar/sidebar.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [MatListModule, MatIconModule, RouterModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
-
+  menu = [
+    { icon: 'dashboard', label: 'Dashboard', route: '/dashboard' },
+    { icon: 'account_balance', label: 'Empréstimos', route: '/loans' },
+    { icon: 'people', label: 'Clientes', route: '/clients' },
+    { icon: 'payments', label: 'Pagamentos', route: '/payments' },
+    { icon: 'bar_chart', label: 'Relatórios', route: '/reports' },
+    { icon: 'settings', label: 'Configurações', route: '/settings' },
+  ];
 }

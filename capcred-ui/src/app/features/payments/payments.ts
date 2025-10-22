a74ed8ccac57697218bcab8d195273c9bc3ef141
Caio Victor <caiopvasconcelos@gmail.com>
2025-10-21 23:21:13 -0300

    feat: implementando filtro

--- capcred-ui/src/app/features/payments/payments.ts
+++ capcred-ui/src/app/features/payments/payments.ts
@@ -8,22 +8,20 @@ import { PAYMENTS_MOCK } from '../../core/mocks/payments.mock';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MockDataService } from '../../core/services/mock-data.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-payments',
   standalone: true,
-  imports: [CommonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatButtonModule,
-    MatIconModule,],
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule, MatPaginator, MatSort, MatFormFieldModule, MatInputModule],
   templateUrl: './payments.html',
   styleUrl: './payments.scss'
 })
 export class Payments {
   cols = ['id', 'client', 'value', 'dueDate', 'status', 'actions'];
   dataSource = new MatTableDataSource<any>([]);
+  filterValue = '';
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -39,5 +37,16 @@ export class Payments {
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const value = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.filterValue = value;
+    this.dataSource.filter = value;
+  }
+
+  clearFilter() {
+    this.filterValue = '';
+    this.dataSource.filter = '';
   }
 }

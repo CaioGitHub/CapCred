686af34843115b6435bbac912019aca6c7544a43
Caio Victor <caiopvasconcelos@gmail.com>
2025-10-22 02:42:28 -0300

    feat: ajuste login e sidebar

---
+++ capcred-ui/src/app/features/login/login.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+  ],
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss'],
+})
+export class Login {
+  email = '';
+  password = '';
+  loading = false;
+  error = '';
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.loading = true;
+    this.error = '';
+
+    this.auth.login(this.email, this.password).subscribe((user) => {
+      this.loading = false;
+      if (user) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.error = 'Credenciais inválidas';
+      }
+    });
+  }
+}

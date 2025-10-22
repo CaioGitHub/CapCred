251a29b74d438cd700a4a89dcec208df111a1d1e
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-22 20:10:11 -0300

    feat: add loan access validator

--- ms-loan/src/main/java/com/capcredit/ms_loan/config/security/SecurityConfig.java
+++ ms-loan/src/main/java/com/capcredit/ms_loan/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.capcredit.ms_loan.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,6 +11,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
---
+++ ms-loan/src/main/java/com/capcredit/ms_loan/domain/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.capcredit.ms_loan.domain.enums;
+
+public enum UserRole {
+    CLIENTE, ADMIN;
+}

0fba74f2eab7aff7c3d0061c067ef9bba6f64e2f
tatianambmorais <tatianambmorais@gmail.com>
2025-10-23 17:19:14 -0300

    fix: corrigindo mensagem para implementar o pagamento da parcela

--- ms-notification/src/main/java/com/capcredit/ms_notification/MsNotificationApplication.java
+++ ms-notification/src/main/java/com/capcredit/ms_notification/MsNotificationApplication.java
@@ -2,6 +2,7 @@ package com.capcredit.ms_notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class MsNotificationApplication {

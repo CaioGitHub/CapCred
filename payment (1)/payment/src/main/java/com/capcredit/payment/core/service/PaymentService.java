2fe7814fe5c77da4ea242e9f06ab24409a566234
tatianambmorais <tatianambmorais@gmail.com>
2025-10-23 10:23:25 -0300

    feat: iniciando ms de payment

--- payment (1)/payment/src/main/java/com/capcredit/payment/core/service/PaymentService.java
+++ payment (1)/payment/src/main/java/com/capcredit/payment/core/service/PaymentService.java
@@ -1,11 +1,10 @@
 package com.capcredit.payment.core.service;
 
-import com.capcredit.payment.core.domain.model.Installment;
 import com.capcredit.payment.port.out.dto.InstallmentDTO;
 import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
 
 public interface PaymentService {
-    public ResponseEntity<InstallmentDTO> processPayment(UUID installmentId);
+    ResponseEntity<InstallmentDTO> processPayment(UUID installmentId);
 }

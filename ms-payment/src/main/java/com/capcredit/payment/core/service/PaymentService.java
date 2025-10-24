66b3ca65f2cd378b78a47a20022b58a8d07fa8e8
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-24 15:18:13 -0300

    feat: add method to create installments

--- ms-payment/src/main/java/com/capcredit/payment/core/service/PaymentService.java
+++ ms-payment/src/main/java/com/capcredit/payment/core/service/PaymentService.java
@@ -1,10 +1,11 @@
 package com.capcredit.payment.core.service;
 
+import com.capcredit.payment.core.domain.model.Loan;
 import com.capcredit.payment.port.out.dto.InstallmentDTO;
-import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
 
 public interface PaymentService {
    InstallmentDTO processPayment(UUID installmentId);
+   void createInstallments(Loan loan);
 }

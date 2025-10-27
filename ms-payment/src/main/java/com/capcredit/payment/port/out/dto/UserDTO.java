d4d9b0b5afce8d29b75a6be1c2ad13bf46899a6b
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-27 11:12:59 -0300

    feat: add user model

--- ms-payment/src/main/java/com/capcredit/payment/port/out/dto/UserDTO.java
+++ ms-payment/src/main/java/com/capcredit/payment/port/out/dto/UserDTO.java
@@ -3,6 +3,8 @@ package com.capcredit.payment.port.out.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.capcredit.payment.core.domain.model.User;
+
 public record UserDTO(
 	UUID eventId,
 	LocalDateTime timestamp,
@@ -10,4 +12,14 @@ public record UserDTO(
 	String name,
 	String email,
 	String phone
-) {}
+) {
+	public static UserDTO fromDomain(User user) {
+		return new UserDTO(
+			UUID.randomUUID(),
+			LocalDateTime.now(),
+			user.getId(),
+			user.getName(),
+			user.getEmail(),
+			user.getPhone());
+	}
+}

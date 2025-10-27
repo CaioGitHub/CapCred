c60cdf8bfc3c0cff93c341bac74a2aaead5f7d5a
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-27 11:26:16 -0300

    feat: publish loan completed event when pay all installments

--- ms-payment/src/main/java/com/capcredit/payment/core/service/PaymentServiceImpl.java
+++ ms-payment/src/main/java/com/capcredit/payment/core/service/PaymentServiceImpl.java
@@ -5,10 +5,14 @@ import com.capcredit.payment.core.domain.model.Loan;
 import com.capcredit.payment.core.domain.model.PaymentStatus;
 import com.capcredit.payment.core.exception.InstallmentAlreadyPaidException;
 import com.capcredit.payment.core.exception.InstallmentNotFoundException;
+import com.capcredit.payment.core.exception.UserNotFoundException;
 import com.capcredit.payment.core.mapper.InstallmentMapper;
 import com.capcredit.payment.port.out.InstallmentRepository;
 import com.capcredit.payment.port.out.RabbitMqSender;
 import com.capcredit.payment.port.out.dto.InstallmentDTO;
+import com.capcredit.payment.port.out.dto.LoanCompletedDTO;
+import com.capcredit.payment.port.out.dto.UserDTO;
+
 import org.springframework.stereotype.Service;
 
 import static com.capcredit.payment.core.domain.model.PaymentStatus.PENDING;
@@ -18,17 +22,17 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
     private final InstallmentRepository installmentRepository;
     private final RabbitMqSender rabbitMqSender;
-    public PaymentServiceImpl(InstallmentRepository installmentRepository, RabbitMqSender rabbitMqSender) {
-        this.installmentRepository = installmentRepository;
-        this.rabbitMqSender = rabbitMqSender;
-    }
+    private final UserClient useClient;
 
     public InstallmentDTO processPayment(UUID installmentId) {
         log.info("Processing payment for installmentId={}", installmentId);
@@ -46,6 +50,13 @@ public class PaymentServiceImpl implements PaymentService {
 
         log.info("Installment {} marked as PAID. Sending payment event...", installmentId);
         rabbitMqSender.sendPaymentEvent(InstallmentMapper.toDTO(installment));
+
+        if(!installmentRepository.existsByLoanIdAndPaymentStatus(installment.getLoanId(), PENDING)) {
+            log.info("Loan with id {} was completed, all installments are PAID.", installment.getLoanId());
+            var user = useClient.findById(installment.getUserId())
+                .orElseThrow(() -> new UserNotFoundException("User with ID {} not found for installment {}.".formatted(installment.getUserId(), installmentId)));
+            rabbitMqSender.sendLoanCompletedEvent(LoanCompletedDTO.fromDomain(installment, UserDTO.fromDomain(user)));
+        }
         
         return InstallmentMapper.toDTO(installment);
     }

6ddf92d5e4c910fcaf089456c6601ea40e159bda
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-27 15:06:52 -0300

    feat: add access validation to verify user permission before pay installment

--- ms-payment/src/main/java/com/capcredit/payment/adapters/in/web/PaymentController.java
+++ ms-payment/src/main/java/com/capcredit/payment/adapters/in/web/PaymentController.java
@@ -3,6 +3,7 @@ package com.capcredit.payment.adapters.in.web;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,11 +31,13 @@ public class PaymentController implements PaymentPortIn {
     private final PaymentService paymentService;
 
     @PutMapping("/{installmentId}/pay")
+    @PreAuthorize("@accessValidator.canAccess(#installmentId, authentication)")
     @Operation(summary = "Process payment for an installment")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Loan found", content = {
             @Content(mediaType = "application/json", schema = @Schema(implementation = Loan.class))
         }),
+        @ApiResponse(responseCode = "403", description = "Not has permission to pay installment", content = @Content),
         @ApiResponse(responseCode = "404", description = "Entity not found", content = @Content),
         @ApiResponse(responseCode = "409", description = "Installment already paid", content = @Content)
     })

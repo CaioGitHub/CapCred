611dff71ad8bef3369c9b8a0111c60503166e1d9
Gabriel Neves <gabriel.santos@capgemini.com>
2025-10-28 16:22:19 -0300

    refactor: receive user DTO instead only ID in receive payment event

--- ms-notification/src/main/java/com/capcredit/ms_notification/adapter/out/PaymentReceivedNotificationImpl.java
+++ ms-notification/src/main/java/com/capcredit/ms_notification/adapter/out/PaymentReceivedNotificationImpl.java
@@ -1,47 +1,34 @@
 package com.capcredit.ms_notification.adapter.out;
 
+import org.springframework.stereotype.Component;
+
 import com.capcredit.ms_notification.application.service.EmailService;
 import com.capcredit.ms_notification.application.service.SMSService;
 import com.capcredit.ms_notification.core.domain.Email;
 import com.capcredit.ms_notification.core.domain.SMS;
-import com.capcredit.ms_notification.core.domain.User;
-import com.capcredit.ms_notification.port.out.UserRepository;
 import com.capcredit.ms_notification.port.in.PaymentReceivedPortIn;
 import com.capcredit.ms_notification.port.out.dtos.PaymentReceivedDTO;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class PaymentReceivedNotificationImpl implements PaymentReceivedPortIn {
 
     public static final String SUBJECT_RECEIVED_PAYMENT = "Recebemos seu pagamento!";
 
     private final EmailService emailService;
     private final SMSService smsService;
-    private final UserRepository userRepository;
-
-    private User user;
-
-    public PaymentReceivedNotificationImpl(EmailService emailService,
-                                           SMSService smsService,
-                                           UserRepository userRepository) {
-        this.emailService = emailService;
-        this.smsService = smsService;
-        this.userRepository = userRepository;
-    }
 
     @Override
     public void receivePayment(PaymentReceivedDTO dto) {
-        this.user = getUser(dto);
-
         notifyUserCreationByEmail(dto);
         notifyUserCreationBySMS(dto);
     }
 
     private void notifyUserCreationByEmail(PaymentReceivedDTO dto) {
         Email email = Email.builder()
-                .to(user.getEmail())
+                .to(dto.user().email())
                 .subject(SUBJECT_RECEIVED_PAYMENT)
                 .body(getBody(dto))
                 .build();
@@ -51,26 +38,17 @@ public class PaymentReceivedNotificationImpl implements PaymentReceivedPortIn {
 
     private void notifyUserCreationBySMS(PaymentReceivedDTO dto) {
         SMS sms = SMS.builder()
-                .toNumber(user.getPhone())
+                .toNumber(dto.user().phone())
                 .message(getBody(dto))
                 .build();
 
         smsService.send(sms);
     }
 
-    // TODO trazer número da parcela na mensagem
     private String getBody(PaymentReceivedDTO dto) {
-        return "Olá, " + user.getName() +
+        return "Olá, " + dto.user().name() +
                 ", recebemos o pagamento no valor de R$ " + dto.valuePaid() +
                 " referente à parcela nº " + dto.installmentNumber() +
                 " do empréstimo " + dto.loanId() + ". Obrigado por pagar em dia!";
-    }
-
-    private User getUser(PaymentReceivedDTO dto) {
-        return userRepository.findById(dto.userId())
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND,
-                        "Usuário não encontrado para o ID: " + dto.userId()
-                ));
     }
 }
--- ms-notification/src/test/java/com/capcredit/ms_notification/adapter/out/PaymentReceivedNotificationImplTest.java
+++ ms-notification/src/test/java/com/capcredit/ms_notification/adapter/out/PaymentReceivedNotificationImplTest.java
@@ -4,9 +4,9 @@ import com.capcredit.ms_notification.application.service.EmailService;
 import com.capcredit.ms_notification.application.service.SMSService;
 import com.capcredit.ms_notification.core.domain.Email;
 import com.capcredit.ms_notification.core.domain.SMS;
-import com.capcredit.ms_notification.core.domain.User;
-import com.capcredit.ms_notification.port.out.UserRepository;
 import com.capcredit.ms_notification.port.out.dtos.PaymentReceivedDTO;
+import com.capcredit.ms_notification.port.out.dtos.UserDTO;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -14,7 +14,6 @@ import org.mockito.ArgumentCaptor;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,40 +24,29 @@ class PaymentReceivedNotificationImplTest {
 
     private EmailService emailService;
     private SMSService smsService;
-    private UserRepository userRepository;
     private PaymentReceivedNotificationImpl paymentReceivedNotification;
 
     @BeforeEach
     void setUp() {
         emailService = mock(EmailService.class);
         smsService = mock(SMSService.class);
-        userRepository = mock(UserRepository.class);
-        paymentReceivedNotification = new PaymentReceivedNotificationImpl(emailService, smsService, userRepository);
+        paymentReceivedNotification = new PaymentReceivedNotificationImpl(emailService, smsService);
     }
 
     @Test
     void shouldSendEmailAndSMSWhenPaymentIsReceived() {
-        UUID userId = UUID.randomUUID();
-
-        User user = User.builder()
-                .userId(userId)
-                .name("Carlos Eduardo")
-                .email("carlos@exemplo.com")
-                .phone("+5511988776655")
-                .build();
-
-        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        UserDTO user = new UserDTO(null, null, UUID.randomUUID(), "Carlos Eduardo", "carlos@exemplo.com", "+5511988776655");
 
         PaymentReceivedDTO dto = new PaymentReceivedDTO(
-                UUID.randomUUID(),        // installmentId
-                UUID.randomUUID(),        // loanId
-                5,                        // installmentNumber
-                new BigDecimal("1500.00"),// valueDue
-                LocalDate.now().plusDays(5), // dueDate
-                LocalDateTime.now(),      // paymentDate
-                new BigDecimal("1500.00"),// valuePaid
-                "PAID",                   // paymentStatus
-                userId                    // userId
+                UUID.randomUUID(),
+                UUID.randomUUID(),
+                5,
+                new BigDecimal("1500.00"),
+                LocalDate.now().plusDays(5),
+                LocalDateTime.now(),
+                new BigDecimal("1500.00"),
+                "PAID",
+                user
         );
 
         paymentReceivedNotification.receivePayment(dto);

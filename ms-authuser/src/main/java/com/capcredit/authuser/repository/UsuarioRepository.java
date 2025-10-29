5af16ad36787ef63becf192b94f226b50c1ecd27
Francisco Lima <franciscosouzalima@gmail.com>
2025-10-29 19:42:40 -0300

    feat: refactor authuser service package structure and add Dockerfile for application deployment

---
+++ ms-authuser/src/main/java/com/capcredit/authuser/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.capcredit.authuser.repository;
+
+import com.capcredit.authuser.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    Optional<Usuario> findByEmail(String email);
+}
